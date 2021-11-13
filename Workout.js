ebeddc284b3e806bf9d2ed9688498c8b4e11e251
Will Phenicie <78045357+WillPhen@users.noreply.github.com>
2021-11-13 01:27:01 -0600

    Add files via upload

---
+++ Workout.js
@@ -0,0 +1,31 @@
+var upperChest = ["Incline Bench", "Incline DB Bench", "Upper Chest Cable"]
+var lowerChest = ["Decline Bench", "Decline DB Bench", "Lower Chest Cable"]
+var Chest = ["Bench", "Machine Bench", "DB Bench"]
+
+var tris = ["Cable Pulldowns", "Ski Flies", "Overhead Cable Pulls"]
+
+var back = ["Machine Row", "Straight Bar Pulldown"]
+var latBack = ["Cable Pulldown", "One Hand Machine Rows"]
+
+var longBis = ["Close Grip DB Curl", "Close Grip Preacher Curl"]
+var shortBis = ["DB Curl", "Barbell Curl", "Cable Curl"]
+var brachBis = ["DB Hammer Curls", "Cable Rope Curls", "Barbell Reverse Grip Curls"]
+
+var shoulders = ["DB Shoulder Press", "Barbell Shoulder Press", "Shoulder Cable Side Lifts"]
+var frontShoulders = ["DB Front Raises", "Barbell Front Raises", "Cable Front Raises"]
+var backShoulders = ["Cable Crossover Pulls", "Cable Face Pulls"]
+var trapsShoulders = ["DB Shoulder Shrugs", "Barbell Shoulder Shrugs", "Cable Shoulder Shrugs"]
+
+var legs = ["Barbell Squats", "Leg Press", "Pause Squats", "Front Squats"]
+var hamLegs = ["Hamstring Curl Machine", "Cable Hamstring Curl"]
+var quadLegs = ["Quad Machine Extensions", "Quad Machine Switch Extensions"]
+
+var abs = ["Hanging Knee Raise", "Hanging Leg Raise", "Hold Ab Curls"]
+
+var topForearms = ["Reverse Grip Barbell Forearm Curl", "Reverse Grip Cable Forearm Curl"]
+var bottomForearms = ["Barbell Forearm Curl", "Cable Forearm Curl"]
+
+function randomize() {
+    
+}
+
