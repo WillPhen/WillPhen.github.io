28616d78ad0f11783355a8738e908196fb14429b
will phenicie <willphen4141@gmail.com>
2021-12-08 15:32:04 -0600

    Workout Project Working

--- javascript/Workout.js
+++ javascript/Workout.js
@@ -1,30 +1,30 @@
 
-var upperChest = ["Incline Bench", "Incline DB Bench", "Upper Chest Cable"]
-var lowerChest = ["Decline Bench", "Decline DB Bench", "Lower Chest Cable"]
-var chest = ["Bench", "Machine Bench", "DB Bench"]
+var upperChest = ["Incline Bench <a href=\"https://www.youtube.com/watch?v=SrqOu55lrYU, target=\"_blank\"\">Video Here</a>", "Incline DB Bench <a href=\"https://www.youtube.com/watch?v=hChjZQhX1Ls, target=\"_blank\"\">Video Here</a>", "Upper Chest Cable <a href=\"https://www.youtube.com/watch?v=-lHRM62oa18, target=\"_blank\"\">Video Here</a>"]
+var lowerChest = ["Decline Bench <a href=\", target=\"_blank\"\">Video Here</a>", "Decline DB Bench <a href=\", target=\"_blank\"\">Video Here</a>", "Lower Chest Cable <a href=\", target=\"_blank\"\">Video Here</a>"]
+var chest = ["Bench <a href=\", target=\"_blank\"\">Video Here</a>", "Machine Bench <a href=\", target=\"_blank\"\">Video Here</a>", "DB Bench <a href=\", target=\"_blank\"\">Video Here</a>"]
 
-var tris = ["Cable Pulldowns", "Ski Flies", "Overhead Cable Pulls", "Tricep Machine", "Single Hand Cable Pulldowns", "Tricep Dips"]
+var tris = ["Cable Pushdowns <a href=\"https://www.youtube.com/watch?v=2-LAMcpzODU, target=\"_blank\"\">Video Here</a>", "Tricep Kickbacks <a href=\"https://www.youtube.com/watch?v=z3sm6YgpJho, target=\"_blank\"\">Video Here</a>", "Overhead Cable Pulls <a href=\"https://www.youtube.com/watch?v=GzmlxvSFE7A, target=\"_blank\"\">Video Here</a>", "Tricep Machine <a href=\"https://www.youtube.com/watch?v=4W31U8tTVAg, target=\"_blank\"\">Video Here</a>", "Single Hand Cable Pulldowns <a href=\"https://www.youtube.com/watch?v=Zl46Cxc4DDs, target=\"_blank\"\">Video Here</a>", "Tricep Dips <a href=\"https://www.youtube.com/watch?v=6MwtkyNC2ZY, target=\"_blank\"\">Video Here</a>"]
 
-var back = ["Machine Row", "Straight Bar Pulldown"]
-var latBack = ["Cable Pulldown", "One Hand Machine Rows"]
+var back = ["Machine Row <a href=\"https://www.youtube.com/watch?v=GZbfZ033f74, target=\"_blank\"\">Video Here</a>", "Dumbell Flies <a href=\"https://www.youtube.com/watch?v=M1CzdNm8huc, target=\"_blank\"\">Video Here</a>", "Machine Reverse Fly <a href=\"https://www.youtube.com/watch?v=T_dkYUgQLCY, target=\"_blank\"\">Video Here</a>"]
+var latBack = ["Lat Pulldown <a href=\"https://www.youtube.com/watch?v=lueEJGjTuPQ, target=\"_blank\"\">Video Here</a>", "One Hand Lat Pulldowns <a href=\"https://www.youtube.com/watch?v=0BT533ueEdI, target=\"_blank\"\">Video Here</a>"]
 
-var longBis = ["Close Grip DB Curl", "Close Grip Preacher Curl"]
-var shortBis = ["DB Curl", "Barbell Curl", "Cable Curl"]
-var brachBis = ["DB Hammer Curls", "Cable Rope Curls", "Barbell Reverse Grip Curls"]
+var longBis = ["Close Grip DB Curl <a href=\"https://www.youtube.com/watch?v=mRACeRDHe24, target=\"_blank\"\">Video Here</a>", "Close Grip Preacher Curl <a href=\"https://www.youtube.com/watch?v=fIWP-FRFNU0, target=\"_blank\"\">Video Here</a>"]
+var shortBis = ["DB Curl <a href=\"https://www.youtube.com/watch?v=ykJmrZ5v0Oo, target=\"_blank\"\">Video Here</a>", "Barbell Curl <a href=\"https://www.youtube.com/watch?v=kwG2ipFRgfo, target=\"_blank\"\">Video Here</a>", "Cable Curl <a href=\"https://www.youtube.com/watch?v=AsAVbj7puKo, target=\"_blank\"\">Video Here</a>"]
+var brachBis = ["DB Hammer Curls <a href=\"https://www.youtube.com/watch?v=zC3nLlEvin4, target=\"_blank\"\">Video Here</a>", "Rope Curls <a href=\"https://www.youtube.com/watch?v=1Quc_tOv97I, target=\"_blank\"\">Video Here</a>", "Barbell Reverse Grip Curls <a href=\"https://www.youtube.com/watch?v=nRgxYX2Ve9w, target=\"_blank\"\">Video Here</a>"]
 
-var shoulders = ["DB Shoulder Press", "Barbell Shoulder Press", "Shoulder Cable Side Lifts"]
-var frontShoulders = ["DB Front Raises", "Barbell Front Raises", "Cable Front Raises"]
-var backShoulders = ["Cable Crossover Pulls", "Cable Face Pulls"]
-var trapsShoulders = ["DB Shoulder Shrugs", "Barbell Shoulder Shrugs", "Cable Shoulder Shrugs"]
+var shoulders = ["DB Shoulder Press <a href=\"https://www.youtube.com/watch?v=qEwKCR5JCog, target=\"_blank\"\">Video Here</a>", "Barbell Shoulder Press <a href=\"https://www.youtube.com/watch?v=2yjwXTZQDDI, target=\"_blank\"\">Video Here</a>", "Lateral Cable Lifts <a href=\"https://www.youtube.com/watch?v=PPrzBWZDOhA, target=\"_blank\"\">Video Here</a>"]
+var frontShoulders = ["DB Front Raises <a href=\"https://www.youtube.com/watch?v=-t7fuZ0KhDA, target=\"_blank\"\">Video Here</a>", "Barbell Front Raises <a href=\"https://www.youtube.com/watch?v=Ofo2DQdT7DA, target=\"_blank\"\">Video Here</a>", "Cable Front Raises <a href=\"https://www.youtube.com/watch?v=H0O4HNvtB70, target=\"_blank\"\">Video Here</a>"]
+var backShoulders = ["Cable Crossover Pulls <a href=\"https://www.youtube.com/watch?v=JENKmsEZQO8, target=\"_blank\"\">Video Here</a>", "Cable Face Pulls <a href=\"https://www.youtube.com/watch?v=V8dZ3pyiCBo, target=\"_blank\"\">Video Here</a>"]
+var trapsShoulders = ["DB Shoulder Shrugs <a href=\"https://www.youtube.com/watch?v=cJRVVxmytaM, target=\"_blank\"\">Video Here</a>", "Barbell Shoulder Shrugs <a href=\"https://www.youtube.com/watch?v=NAqCVe2mwzM, target=\"_blank\"\">Video Here</a>", "Cable Shoulder Shrugs <a href=\"https://www.youtube.com/watch?v=Wab3-dsZohU, target=\"_blank\"\">Video Here</a>"]
 
-var legs = ["Barbell Squats", "Leg Press", "Pause Squats", "Front Squats"]
-var hamLegs = ["Hamstring Curl Machine", "Cable Hamstring Curl"]
-var quadLegs = ["Quad Machine Extensions", "Quad Machine Switch Extensions"]
+var legs = ["Barbell Squats <a href=\"https://www.youtube.com/watch?v=MVMNk0HiTMg, target=\"_blank\"\">Video Here</a>", "Leg Press <a href=\"https://www.youtube.com/watch?v=IZxyjW7MPJQ, target=\"_blank\"\">Video Here</a>", "Pause Squats <a href=\"https://www.youtube.com/watch?v=RH_7Efvn35w, target=\"_blank\"\">Video Here</a>", "Front Squats <a href=\"https://www.youtube.com/watch?v=I5GaxSYLCSc, target=\"_blank\"\">Video Here</a>"]
+var hamLegs = ["Hamstring Curl Machine <a href=\"https://www.youtube.com/watch?v=1Tq3QdYUuHs, target=\"_blank\"\">Video Here</a>", "Cable Hamstring Curl <a href=\"https://www.youtube.com/watch?v=OA7c_8HfdOw, target=\"_blank\"\">Video Here</a>"]
+var quadLegs = ["Leg Extensions <a href=\"https://www.youtube.com/watch?v=YyvSfVjQeL0, target=\"_blank\"\">Video Here</a>", "Step Ups <a href=\"https://www.youtube.com/watch?v=d4p6Jy6KUO4, target=\"_blank\"\">Video Here</a>"]
 
-var abs = ["Hanging Knee Raise", "Hanging Leg Raise", "Hold Ab Curls"]
+var abs = ["Hanging Knee Raise <a href=\"https://www.youtube.com/watch?v=KNzJ3GuIpB8, target=\"_blank\"\">Video Here</a>", "Hanging Leg Raise <a href=\"https://www.youtube.com/watch?v=fQXbAeClHqY, target=\"_blank\"\">Video Here</a>", "Machine Crunches <a href=\"https://www.youtube.com/watch?v=_O1xunCfYEM, target=\"_blank\"\">Video Here</a>", "Russian Twists <a href=\"https://www.youtube.com/watch?v=wkD8rjkodUI, target=\"_blank\"\">Video Here</a>"]
 
-var topForearms = ["Reverse Grip Barbell Forearm Curl", "Reverse Grip Cable Forearm Curl"]
-var bottomForearms = ["Barbell Forearm Curl", "Cable Forearm Curl"]
+var topForearms = ["Reverse Grip Barbell Forearm Curl <a href=\"https://www.youtube.com/watch?v=FW7URAaC-vE, target=\"_blank\"\">Video Here</a>", "Reverse Grip Cable Forearm Curl <a href=\"https://www.youtube.com/watch?v=HwB-DevuJjU, target=\"_blank\"\">Video Here</a>"]
+var bottomForearms = ["Barbell Forearm Curl <a href=\"https://www.youtube.com/watch?v=MvtUIqjRNRc, target=\"_blank\"\">Video Here</a>", "Cable Forearm Curl <a href=\"https://www.youtube.com/watch?v=bjBxUe7zibU, target=\"_blank\"\">Video Here</a>"]
 
 
 var chestClick = document.getElementById("chest")
@@ -38,31 +38,42 @@ var forearmsClick = document.getElementById("fore")
 
 var submit = document.getElementById("create-btn")
 
-//duplicate checker
-//print videos of workouts with each workout
 submit.addEventListener("click", function() {
     //window.open("WorkoutPage.html") 
 
     if(chestClick.checked) {
         document.writeln("<h2>Chest Exercises:</h2>")
-        document.writeln("<p>Chest(Upper): " + upperChest[Math.floor(Math.random() * upperChest.length)] + "</p>")
+        document.writeln("<p>Upper Chest: " + upperChest[Math.floor(Math.random() * upperChest.length)] + "</p>")
         document.writeln("<p>Chest: " + chest[Math.floor(Math.random() * chest.length)] + "</p>")
-        document.writeln("<p>Chest(Lower): " + lowerChest[Math.floor(Math.random() * lowerChest.length)] + "</p>")
+        document.writeln("<p>Lower Chest: " + lowerChest[Math.floor(Math.random() * lowerChest.length)] + "</p>")
         }
 
         if(trisClick.checked) {
-            //var pulledValues = []
-                //first
-                document.writeln("<h2>Triceps Exercises:</h2>")
-                document.writeln("<p>Triceps: " + tris[Math.floor(Math.random() * tris.length)] + "</p>")
-                document.writeln("<p>Triceps: " + tris[Math.floor(Math.random() * tris.length)] + "</p>")
-                document.writeln("<p>Triceps: " + tris[Math.floor(Math.random() * tris.length)] + "</p>")
+            document.writeln("<h2>Triceps Exercises:</h2>")
+            var pulledValues = []
+            
+                while(pulledValues.length != 3) {
+                    idx = Math.floor(Math.random() * tris.length)
+                    var value = tris[idx]
+                    if(!pulledValues.includes(value)) {
+                        document.writeln("<p>Triceps: " + value + "</p>")
+                        pulledValues.push(value)
+                    }
+                }
         }
 
         if(backClick.checked) {
             document.writeln("<h2>Back Exercises:</h2>")
-            document.writeln("<p>Back: " + back[Math.floor(Math.random() * back.length)] + "</p>")
-            document.writeln("<p>Back: " + back[Math.floor(Math.random() * back.length)] + "</p>")
+            var pulledValues = []
+            
+                while(pulledValues.length != 2) {
+                    idx = Math.floor(Math.random() * back.length)
+                    var value = back[idx]
+                    if(!pulledValues.includes(value)) {
+                        document.writeln("<p>Back: " + value + "</p>")
+                        pulledValues.push(value)
+                    }
+                }
             document.writeln("<p>Lats: " + latBack[Math.floor(Math.random() * latBack.length)] + "</p>")
         }
 
@@ -90,9 +101,16 @@ submit.addEventListener("click", function() {
 
         if(absClick.checked) {
             document.writeln("<h2>Ab Exercises:</h2>")
-            document.writeln("<p>Abs: " + abs[Math.floor(Math.random() * abs.length)] + "</p>")
-            document.writeln("<p>Abs: " + abs[Math.floor(Math.random() * abs.length)] + "</p>")
-            document.writeln("<p>Abs: " + abs[Math.floor(Math.random() * abs.length)] + "</p>")
+            var pulledValues = []
+            
+                while(pulledValues.length != 3) {
+                    idx = Math.floor(Math.random() * abs.length)
+                    var value = abs[idx]
+                    if(!pulledValues.includes(value)) {
+                        document.writeln("<p>Abs: " + value + "</p>")
+                        pulledValues.push(value)
+                    }
+                }
         }
 
         if(forearmsClick.checked) {
@@ -100,6 +118,10 @@ submit.addEventListener("click", function() {
             document.writeln("<p>Forearms(top): " + topForearms[Math.floor(Math.random() * topForearms.length)] + "</p>")
             document.writeln("<p>Forearms(bottom): " + bottomForearms[Math.floor(Math.random() * bottomForearms.length)] + "</p>")
         }
+
+        document.writeln("<br>")
+        document.writeln("<b>If you would like to make a different selection refresh the page<b>")
+
 })
 
 
