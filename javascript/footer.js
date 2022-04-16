82fada17eefd5c87583aa65ea13a79aeb71c6fdb
will phenicie <willphen4141@gmail.com>
2022-04-15 22:32:16 -0500

    New Project Completed

---
+++ javascript/footer.js
@@ -0,0 +1,7 @@
+function writeFooter() {
+    document.write("<footer>")
+    document.write("William Phenicie, <a href=\"mailto:willphen4141@gmail.com\">Email me here</a>, <a href=\"https://www.linkedin.com/in/willphenicie/\">LinkedIn</a>, <a href=\"https://github.com/WillPhen/\">GitHub</a>")
+    document.write("</footer>")
+}
+
+writeFooter()
---
+++ javascript/navbar.js
@@ -0,0 +1,21 @@
+function writeNavbar() {
+    document.write("<div class=\"container\">")
+    document.write("<div class=\"navWrapper\">")
+    document.write("<div class=\"leftHeader\">")
+    document.write("<div class=\"navLink\">")
+    document.write("<a href=\"HomePage.html\">Home</a>")
+    document.write("</div>")
+    document.write("<div class=\"navLink active-nav-link\">")
+    document.write("<a href=\"About.html\" id=\"about\">About Me</a>")
+    document.write("</div>")
+    document.write("</div>")
+    document.write("<div class=\"rightHeader\">")
+    document.write("<div class=\"navLink\">")
+    document.write("<a href=\"myInfo.html\">William Phenicie</a>")
+    document.write("</div>")
+    document.write("</div>")
+    document.write("</div>")
+    document.write("</div>")
+}
+
+writeNavbar()
