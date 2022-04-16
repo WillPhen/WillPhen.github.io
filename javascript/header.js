8ececb9ec826834e6afd7eaf7305cc189a728521
will phenicie <willphen4141@gmail.com>
2022-04-16 01:41:43 -0500

    Debugging

--- javascript/header.js
+++ javascript/header.js
@@ -4,9 +4,9 @@ function writeHeader() {
     document.write("<meta charset=\"utf-8\">")
     document.write("<meta name=\"description\" context=\"CIS-Insiders HomePage\">")
     document.write("<meta name=\"author\" content=\"Will Phencie and Zachary Bramlett\">")
-    document.write("<title>CIS-Insiders Home Page</title>")
-    document.write("<link rel=\"stylesheet\" href=\"/WillPhen.github.io/css/defaults.css\">")
-    document.write("<link rel=\"stylesheet\" href=\"/WillPhen.github.io/css/SAPstyles.css\">")
+    document.write("<title>CIS-Insiders Home Page</title>")    
+    document.write("<link href=\"css/defaults.css\" type=\"text/css\" rel=\"stylesheet\">")
+    document.write("<link href=\"css/SAPstyles.css\" type=\"text/css\" rel=\"stylesheet\"></link>")
     document.write("</head>")
 }
 
