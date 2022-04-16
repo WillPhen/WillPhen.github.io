06aa294140ab5885bd0ff8035a46c092da443674
will phenicie <willphen4141@gmail.com>
2022-04-16 02:45:56 -0500

    Debugging

--- javascript/navbarSAP.js
+++ javascript/navbarSAP.js
@@ -1,7 +1,6 @@
 function writeNavbar() {
     document.write("<navbar>")
     document.write("<nav id=\"nav\">")
-    document.write("<div class=\"logo\"><a href=\"SAPindex.html\"><img class=\"logo\" src=\"images/CISInsiders.jpg\" alt=\"CSInsiders\" width=\"500\" height=\"150\"></a></div></img>")       
     document.write("<ul>")
     document.write("<li><a href=\"HomePage.html\">Main Page</a></li>")
     document.write("<li><a href=\"SAPindex.html\">Home</a></li>")
